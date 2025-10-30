68454b1349ec84678b6c3956562f908a8356f965
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-10-30 00:01:17 -0300

    Initial Commit

---
+++ WTCMessenger/WTCMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTCMessengerApp.swift
+//  WTCMessenger
+//
+//  Created by Rodrigo Belarmino de Oliveira on 30/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WTCMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
